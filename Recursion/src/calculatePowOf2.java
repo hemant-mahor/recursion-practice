10b4a07c87a36720de528a3a3df6b1a63c037107
hemant.mahor <hemant.mahor@macys.com>
2024-03-17 20:35:30 +0530

    rec-1:Added calulatePowOf2 code

--- Recursion/src/calculatePowOf2.java
+++ Recursion/src/calculatePowOf2.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class calculatePowOf2 {
+public class calculatePowOf2 {
+    public static void main(String[] args) {
+        System.out.println(pow(4));
+    }
+
+    public static int pow(int n){
+        if(n == 1){
+            return 2;
+        }
+
+        int call = pow(n-1);
+        return call * 2;
+    }
 }
